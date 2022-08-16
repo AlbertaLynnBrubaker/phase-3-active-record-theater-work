0a82b10f10198c2df373c552ec1c5086530f97c5
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-08-16 09:51:31 -0500

    finished deliverables

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ Role.reset_pk_sequence
 puts "loading..."
 
   Audition.create(actor: "Joe", location: "LA", phone: 5555555, hired: false, role_id: 1 )
-  Audition.create(actor: "Jan", location: "SF", phone: 5555551, hired: false, role_id: 1 )
+  Audition.create(actor: "Jan", location: "SF", phone: 5555551, hired: true, role_id: 1 )
   Audition.create(actor: "Alie", location: "MN", phone: 5555552, hired: false, role_id: 1 )
   Audition.create(actor: "Remberto", location: "NY", phone: 5555553, hired: false, role_id: 1 )
   Audition.create(actor: "Jake", location: "FL", phone: 5555554, hired: false, role_id: 1 )
